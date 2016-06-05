22268c0430f98c48975a5c95ce39db2fb613cf36
Oleg Kleyman <Omego2K@gmail.com>
2016-06-05 19:16:07 -0400

    implement constructor

---
+++ core/Omego.Extensions/AttemptCatchIterator.cs
@@ -0,0 +1,34 @@
+﻿namespace Omego.Extensions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    public class AttemptCatchIterator<T> : IEnumerator<T>
+    {
+        public AttemptCatchIterator(IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+
+
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Current { get; }
+
+        object IEnumerator.Current => Current;
+    }
+}
--- tests/unit/Omego.Extensions.Tests.Unit/AttemptCatchIteratorTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/AttemptCatchIteratorTests.cs
@@ -1,7 +1,20 @@
 ﻿namespace Omego.Extensions.Tests.Unit
 {
+    using System;
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using Xunit;
+
     public class AttemptCatchIteratorTests
     {
+        [Fact]
+        public void ConstructorShouldThrowExceptionWhenRequiredArgumentsAreNull()
+        {
+            Action constructor = () => new AttemptCatchIterator<int>(null);
 
+            constructor.ShouldThrow<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("enumerable");
+        }
     }
 }
