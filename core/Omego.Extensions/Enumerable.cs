f39a923dfe43ede3b502795c5fa08bd3782dec16
Oleg Kleyman <Omego2K@gmail.com>
2016-06-12 13:25:10 -0400

    add catch method and test

---
+++ core/Omego.Extensions/Enumerable.cs
@@ -0,0 +1,18 @@
+﻿namespace Omego.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Enumerable
+    {
+        public static IEnumerable<T> Catch<T, TE>(this IEnumerable<T> target, Action<TE> handler) where TE: Exception
+        {
+            var iterator = new AttemptCatchIterator<T, TE>(target, handler);
+
+            iterator.MoveNext();
+
+            yield return iterator.Current;
+        }
+    }
+}
--- tests/unit/Omego.Extensions.Tests.Unit/EnumerableTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/EnumerableTests.cs
@@ -1,6 +1,23 @@
 ﻿namespace Omego.Extensions.Tests.Unit
 {
+    using System;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Xunit;
+
     public class EnumerableTests
     {
+        [Fact]
+        public void CatchShouldCatchException()
+        {
+            var enumerable = new[] { 0 }.Select(i => 1 / i);
+
+            var handled = false;
+
+            enumerable.Catch<int, DivideByZeroException>(ex => handled = true).ToList();
+            handled.Should().BeTrue();
+        }
     }
 }
