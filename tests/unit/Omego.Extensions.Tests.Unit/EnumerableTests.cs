8fd66591282ed954a576bdd297cb314099db4876
Oleg Kleyman <Omego2K@gmail.com>
2016-06-12 13:48:00 -0400

    add unit test for multiple catches

--- tests/unit/Omego.Extensions.Tests.Unit/EnumerableTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/EnumerableTests.cs
@@ -26,5 +26,29 @@
             var enumerable = new[] { -1, 0, 1 }.Select(i => 1 / i);
             enumerable.Catch<int, DivideByZeroException>(e => { }).ShouldAllBeEquivalentTo(new[] { -1, 1 });
         }
+
+        [Fact]
+        public void CatchShouldHandleAllRequestedExceptionsWhenCalledOnAnotherCatch()
+        {
+            var enumerable = new[] { -1, 0, 1 }.Select(
+                i =>
+                    {
+                        if (i == 1)
+                        {
+                            throw new InvalidOperationException();
+                        }
+
+                        return 1 / i;
+                    });
+
+            var dividedByZeroExceptionHandled = false;
+            var invalidOperationExceptionHandled = false;
+
+            enumerable.Catch<int, DivideByZeroException>(ex => dividedByZeroExceptionHandled = true)
+                .Catch<int, InvalidOperationException>(ex => invalidOperationExceptionHandled = true).ToList();
+
+            dividedByZeroExceptionHandled.Should().BeTrue();
+            invalidOperationExceptionHandled.Should().BeTrue();
+        }
     }
 }
