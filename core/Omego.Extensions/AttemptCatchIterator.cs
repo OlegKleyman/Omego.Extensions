e463f0925e8e5863fcc199db2635628554576334
Oleg Kleyman <Omego2K@gmail.com>
2016-06-12 16:07:32 -0400

    add XML doc

--- core/Omego.Extensions/AttemptCatchIterator.cs
+++ core/Omego.Extensions/AttemptCatchIterator.cs
@@ -4,6 +4,11 @@
     using System.Collections;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Represents an enumerator that attempts to catch exceptions.
+    /// </summary>
+    /// <typeparam name="T">The type to iterate.</typeparam>
+    /// <typeparam name="TE">The type of <see cref="Exception"/> to catch.</typeparam>
     public class AttemptCatchIterator<T, TE> : IEnumerator<T> where TE: Exception
     {
         private readonly Action<TE> handler;
@@ -14,6 +19,11 @@
 
         private bool disposed;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttemptCatchIterator{T,TE}"/> class.
+        /// </summary>
+        /// <param name="enumerable">The <see cref="IEnumerable{T}"/> of <see cref="T"/> to iterate on.</param>
+        /// <param name="handler">The <see cref="Action{T}"/> of <see cref="TE"/> to call when a(n) <see cref="TE"/> exception occurrs.</param>
         public AttemptCatchIterator(IEnumerable<T> enumerable, Action<TE> handler)
         {
             if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
@@ -23,12 +33,16 @@
             this.handler = handler;
         }
 
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
         public void Dispose()
         {
             current = default(T);
             disposed = true;
         }
 
+        /// <summary>Advances the enumerator to the next element of the collection.</summary>
+        /// <returns>true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.</returns>
+        /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception>
         public bool MoveNext()
         {
             var success = false;
@@ -53,15 +67,26 @@
             return success;
         }
 
+        /// <summary>Sets the enumerator to its initial position, which is before the first element in the collection.</summary>
+        /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception>
+        /// <remarks>Not supported.</remarks>
         public void Reset()
         {
             throw new NotSupportedException();
         }
 
+        /// <summary>Gets the element in the collection at the current position of the enumerator.</summary>
+        /// <returns>The element in the collection at the current position of the enumerator.</returns>
         public T Current => current;
 
+        /// <summary>Gets the current element in the collection.</summary>
+        /// <returns>The current element in the collection.</returns>
         object IEnumerator.Current => Current;
 
+        /// <summary>
+        /// Gets <see cref="ExceptionOccured"/>.
+        /// </summary>
+        /// <value>Returns <c>true</c> if the last iteration had an exception.</value>
         public bool ExceptionOccured { get; private set; }
     }
 }
