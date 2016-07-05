29c72f9844e3eebb4643072b3bdd118713529948
Oleg Kleyman <Omego2K@gmail.com>
2016-07-05 16:47:10 -0400

    add first or throw by query for queryable

---
+++ core/Omego.Extensions/Queryable.cs
@@ -0,0 +1,24 @@
+﻿namespace Omego.Extensions
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public static class Queryable
+    {
+        public static T FirstOrThrow<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, Exception exception)
+        {
+            if (queryable == null) throw new ArgumentNullException(nameof(queryable));
+            if(predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            if (!queryable.Any(predicate))
+            {
+                if (exception == null) throw new ArgumentNullException(nameof(exception));
+
+                throw exception;
+            }
+
+            return queryable.First(predicate);
+        }
+    }
+}
--- tests/unit/Omego.Extensions.Tests.Unit/FirstQueryableExtensionsTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/FirstQueryableExtensionsTests.cs
@@ -1,6 +1,56 @@
 ﻿namespace Omego.Extensions.Tests.Unit
 {
+    using System;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Xunit;
+
     public class FirstQueryableExtensionsTests
     {
+        [Fact]
+        public void FirstOrThrowShouldReturnElementByQueryWhenFound()
+        {
+            var queryable = new[] { 1 }.AsQueryable();
+
+            queryable.FirstOrThrow(x => x == 1, null).Should().Be(1);
+        }
+
+        [Fact]
+        public void FirstOrThrowShouldThrowArgumentNullExceptionWhenEnumerableArgumentIsNullWhenSearchingByQuery()
+        {
+            Action firstOrThrow = () => ((IQueryable<int>)null).FirstOrThrow(x => false, null);
+
+            firstOrThrow.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("queryable");
+        }
+
+        [Fact]
+        public void FirstOrThrowShouldThrowArgumentNullExceptionWhenExceptionArgumentIsNullWhenQueryIsNotFound()
+        {
+            Action firstOrThrow = () => new object[0].AsQueryable().FirstOrThrow(x => false, null);
+
+            firstOrThrow.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("exception");
+        }
+
+        [Fact]
+        public void FirstOrThrowShouldThrowArgumentNullExceptionWhenPredicateArgumentIsNullWhenSearchingByQuery()
+        {
+            Action firstOrThrow = () => new[] { 1 }.AsQueryable().FirstOrThrow(null, null);
+
+            firstOrThrow.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("predicate");
+        }
+
+        [Fact]
+        public void FirstOrThrowShouldThrowExceptionWhenAnElementByQueryIsNotFound()
+        {
+            var queryable = new[] { 1 }.AsQueryable();
+
+            var ex = new InvalidOperationException();
+
+            Action firstOrThrow = () => queryable.FirstOrThrow(x => x == 0, ex);
+
+            firstOrThrow.ShouldThrowExactly<InvalidOperationException>().Which.Should().Be(ex);
+        }
     }
 }
