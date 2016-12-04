0b71ffdbc4d9d2c475ecb4199d8b6227b90d53e0
Oleg Kleyman <Omego2K@gmail.com>
2016-12-04 01:56:21 -0500

    add Element equality and conversion functions

--- core/Omego.Extensions/Element.cs
+++ core/Omego.Extensions/Element.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public struct Element<T>
+    public struct Element<T> : IEquatable<Element<T>>, IEquatable<T>
     {
         private readonly T value;
 
@@ -22,6 +22,56 @@
 
                 return value;
             }
+        }
+
+        public bool Equals(Element<T> other)
+        {
+            return Present == other.Present && (!Present || Equals(other.Value));
+        }
+
+        public bool Equals(T other)
+        {
+            return Present && (Value != null ? Value.Equals(other) : value == null && other == null);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Element<T> && Equals((Element<T>)obj)) || (obj is T && Equals((T)obj));
+        }
+
+        public override int GetHashCode()
+        {
+            return !Present ? 0 : (value != null ? value.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Element<T> first, Element<T> second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Element<T> first, Element<T> second)
+        {
+            return !(first == second);
+        }
+
+        public static bool operator ==(Element<T> first, T second)
+        {
+            return first.Present && first.Equals(second);
+        }
+
+        public static bool operator !=(Element<T> first, T second)
+        {
+            return !(first == second);
+        }
+
+        public static implicit operator Element<T>(T target)
+        {
+            return new Element<T>(target);
+        }
+
+        public static explicit operator T(Element<T> target)
+        {
+            return target.Value;
         }
     }
 }
