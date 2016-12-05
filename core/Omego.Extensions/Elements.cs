0b7bf5811d894e69b9b392034f8972854b25bed2
Oleg Kleyman <Omego2K@gmail.com>
2016-12-04 20:08:17 -0500

    complete XML doc for Elements

--- core/Omego.Extensions/Elements.cs
+++ core/Omego.Extensions/Elements.cs
@@ -1,11 +1,23 @@
 ﻿namespace Omego.Extensions
 {
+    /// <summary>
+    /// Represents a quantity category of elements.
+    /// </summary>
     public enum Elements
     {
+        /// <summary>
+        /// Represents no elements.
+        /// </summary>
         None,
 
+        /// <summary>
+        /// Represents one element.
+        /// </summary>
         One,
 
+        /// <summary>
+        /// Represents multiple elements.
+        /// </summary>
         Multiple
     }
 }
