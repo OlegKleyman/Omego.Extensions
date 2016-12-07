3a1775e79dbb5c5847f519b79624dfa67eb502b8
Oleg Kleyman <Omego2K@gmail.com>
2016-12-07 15:47:51 -0500

    add FirstOr lazy methods

--- core/Omego.Extensions/QueryableExtensions/FirstOr.cs
+++ core/Omego.Extensions/QueryableExtensions/FirstOr.cs
@@ -20,11 +20,7 @@
         /// <returns>An instance of <typeparamref name="T" />.</returns>
         /// <exception cref="ArgumentNullException"></exception>
         public static T FirstOr<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, T @default)
-        {
-            var element = queryable.FirstElement(predicate);
-
-            return element.Present ? element.Value : @default;
-        }
+            => queryable.FirstOr(predicate, () => @default);
 
         /// <summary>
         ///     Returns the first element of an <see cref="IQueryable{T}" /> matching the given predicate or returns
@@ -34,11 +30,35 @@
         /// <param name="default">The object to return if no elements are found.</param>
         /// <typeparam name="T">The type of the object to return.</typeparam>
         /// <returns>An instance of <typeparamref name="T" />.</returns>
-        public static T FirstOr<T>(this IQueryable<T> queryable, T @default)
-        {
-            var element = queryable.FirstElement(arg => true);
+        public static T FirstOr<T>(this IQueryable<T> queryable, T @default) => queryable.FirstOr(arg => true, @default);
 
-            return element.Present ? element.Value : @default;
-        }
+        /// <summary>
+        ///     Returns the first element of an <see cref="IQueryable{T}" /> matching the given predicate or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="queryable">The queryable to find the first element in.</param>
+        /// <param name="predicate">The predicate to use to find the first element.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static T FirstOr<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, Func<T> @default)
+            => queryable.FirstElement(predicate).ValueOr(@default);
+
+        /// <summary>
+        ///     Returns the first element of an <see cref="IQueryable{T}" /> matching the given predicate or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="queryable">The queryable to find the first element in.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
+        public static T FirstOr<T>(this IQueryable<T> queryable, Func<T> @default) => queryable.FirstOr(arg => true, @default);
     }
 }
--- tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/FirstOrTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/FirstOrTests.cs
@@ -16,7 +16,7 @@
         {
             var queryable = new[] { "1" }.AsQueryable();
 
-            queryable.FirstOr(x => x == "1", null).Should().Be("1");
+            queryable.FirstOr(x => x == "1", (string)null).Should().Be("1");
         }
 
         [Fact]
@@ -40,13 +40,13 @@
         {
             var queryable = new[] { "1" }.AsQueryable();
 
-            queryable.FirstOr(null).Should().Be("1");
+            queryable.FirstOr((string)null).Should().Be("1");
         }
 
         [Fact]
         public void FirstOrShouldThrowArgumentNullExceptionWhenEnumerableArgumentIsNull()
         {
-            Action firstOrThrow = () => ((IQueryable<string>)null).FirstOr(null);
+            Action firstOrThrow = () => ((IQueryable<string>)null).FirstOr((string)null);
 
             firstOrThrow.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("queryable");
         }
@@ -54,7 +54,7 @@
         [Fact]
         public void FirstOrShouldThrowArgumentNullExceptionWhenEnumerableArgumentIsNullWhenSearchingByQuery()
         {
-            Action firstOrThrow = () => ((IQueryable<string>)null).FirstOr(x => false, null);
+            Action firstOrThrow = () => ((IQueryable<string>)null).FirstOr(x => false, (string)null);
 
             firstOrThrow.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("queryable");
         }
@@ -62,9 +62,67 @@
         [Fact]
         public void FirstOrShouldThrowArgumentNullExceptionWhenPredicateArgumentIsNullWhenSearchingByQuery()
         {
-            Action firstOr = () => new string[] { null }.AsQueryable().FirstOr(null, null);
+            Action firstOr = () => new string[] { null }.AsQueryable().FirstOr(null, (string)null);
 
             firstOr.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("predicate");
+        }
+        
+        [Fact]
+        public void FirstOrLazyByQueryShouldArgumentNullExceptionWhenDefaultFuncIsNull()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            Action firstOr = () => queryable.FirstOr(x => x == "2", (Func<string>)null);
+
+            firstOr.ShouldThrow<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("default");
+        }
+
+        [Fact]
+        public void FirstOrLazyByQueryShouldReturnElementWhenFound()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.FirstOr(x => x == "1", (Func<string>)null).Should().Be("1");
+        }
+
+        [Fact]
+        public void FirstOrLazyByQueryShouldReturnRequestedDefaultObjectWhenEnumerableIsEmpty()
+        {
+            var queryable = new string[0].AsQueryable();
+
+            queryable.FirstOr(s => false, () => "3").Should().Be("3");
+        }
+
+        [Fact]
+        public void FirstOrLazyByQueryShouldReturnRequestedDefaultObjectWhenNotFound()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.FirstOr(x => x == "2", () => "3").Should().Be("3");
+        }
+
+        [Fact]
+        public void FirstOrLazyShouldReturnElementWhenFound()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.FirstOr((Func<string>)null).Should().Be("1");
+        }
+
+        [Fact]
+        public void FirstOrLazyShouldReturnRequestedDefaultObjectWhenEnumerableIsEmpty()
+        {
+            var queryable = new string[0].AsQueryable();
+
+            queryable.FirstOr(() => "3").Should().Be("3");
+        }
+
+        [Fact]
+        public void FirstOrLazyShouldReturnRequestedDefaultObjectWhenNotFound()
+        {
+            var queryable = new string[0].AsQueryable();
+
+            queryable.FirstOr(() => "3").Should().Be("3");
         }
     }
 }
