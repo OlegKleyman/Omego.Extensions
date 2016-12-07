53537f311e16d85e1d3e30f9da3b80b65555ee45
Oleg Kleyman <Omego2K@gmail.com>
2016-12-07 15:03:12 -0500

    add XML doc to FirstOr method

--- core/Omego.Extensions/EnumerableExtensions/FirstOr.cs
+++ core/Omego.Extensions/EnumerableExtensions/FirstOr.cs
@@ -17,7 +17,6 @@
         /// <param name="default">The object to return if no elements are found.</param>
         /// <typeparam name="T">The type of the object to return.</typeparam>
         /// <returns>An instance of <typeparamref name="T" />.</returns>
-        /// <exception cref="ArgumentNullException"></exception>
         public static T FirstOr<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate, T @default)
         {
             return enumerable.FirstOr(predicate, () => @default);
@@ -36,11 +35,34 @@
             return enumerable.FirstOr(arg => true, @default);
         }
 
+        /// <summary>
+        ///     Returns the first element of an <see cref="IEnumerable{T}" /> matching the given predicate or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="enumerable">The enumerable to find the first element in.</param>
+        /// <param name="predicate">The predicate to use to find the first element.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
         public static T FirstOr<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate, Func<T> @default)
         {
             return enumerable.FirstElement(predicate).ValueOr(@default);
         }
 
+        /// <summary>
+        ///     Returns the first element of an <see cref="IEnumerable{T}" /> matching the given predicate or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="enumerable">The enumerable to find the first element in.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
         public static T FirstOr<T>(this IEnumerable<T> enumerable, Func<T> @default)
         {
             return enumerable.FirstOr(x => true, @default);
