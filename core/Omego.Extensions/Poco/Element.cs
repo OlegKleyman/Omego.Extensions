df7b09c4b6a8c5cd04b4ce732054bae95060419a
Oleg Kleyman <Omego2K@gmail.com>
2016-12-07 14:47:41 -0500

    add ValueOf documentation

--- core/Omego.Extensions/Poco/Element.cs
+++ core/Omego.Extensions/Poco/Element.cs
@@ -149,6 +149,12 @@
             return target.Value;
         }
 
+        /// <summary>
+        /// Gets the value of this element or <paramref name="default"/> if no value exists.
+        /// </summary>
+        /// <param name="default">The default value to return if one does not exist.</param>
+        /// <returns>An instance or value of <typeparamref name="T"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="default"/> is null.</exception>
         public T ValueOr(Func<T> @default)
         {
             Func<T> defaultSelector = () =>
--- core/Omego.Extensions/Poco/SingleElementResult.cs
+++ core/Omego.Extensions/Poco/SingleElementResult.cs
@@ -203,6 +203,7 @@
         /// <param name="default">The default value to return if one does not exist.</param>
         /// <returns>An instance or value of <typeparamref name="T"/>.</returns>
         /// <exception cref="InvalidOperationException">Thrown when multiple elements exist.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="default"/> is null.</exception>
         public T ValueOr(Func<T> @default)
         {
             Func<T> defaultSelector = () =>
