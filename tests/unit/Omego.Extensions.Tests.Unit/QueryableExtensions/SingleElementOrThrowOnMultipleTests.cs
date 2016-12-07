5d8b203ffbb04929cad182c4dffd564c4b58741b
Oleg Kleyman <Omego2K@gmail.com>
2016-12-07 11:36:29 -0500

    restructure single queryable tests

--- tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/SingleElementOrThrowOnMultipleTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/SingleElementOrThrowOnMultipleTests.cs
@@ -1,6 +1,76 @@
 ﻿namespace Omego.Extensions.Tests.Unit.QueryableExtensions
 {
+    using System;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Omego.Extensions.Poco;
+    using Omego.Extensions.QueryableExtensions;
+
+    using Xunit;
+
     public class SingleElementOrThrowOnMultipleTests
     {
+        [Fact]
+        public void SingleElementOrThrowOnMultipleByQueryShouldReturnElementWhenFound()
+        {
+            var queryable = new[] { 1 }.AsQueryable();
+
+            queryable.SingleElementOrThrowOnMultiple(x => x == 1, null).Value.Should().Be(1);
+        }
+
+        [Fact]
+        public void SingleElementOrThrowOnMultipleByQueryShouldReturnNoMatchesFlagWhenElementIsNotFound()
+        {
+            var queryable = new int[0].AsQueryable();
+
+            queryable.SingleElementOrThrowOnMultiple(x => false, null).Should().Be(SingleElementResult<int>.NoElements);
+        }
+
+        [Fact]
+        public void SingleElementOrThrowOnMultipleByQueryShouldThrowExceptionWhenMultipleElementsAreFound()
+        {
+            var queryable = new[] { 1, 2 }.AsQueryable();
+
+            var ex = new InvalidOperationException();
+
+            Action singleElementOrThrowOnMultiple = () => queryable.SingleElementOrThrowOnMultiple(i => true, ex);
+
+            singleElementOrThrowOnMultiple.ShouldThrow<InvalidOperationException>().Which.Should().Be(ex);
+        }
+
+        [Fact]
+        public void SingleElementOrThrowOnMultipleByQueryShouldThrowExceptionWhenMultipleElementsExceptionIsNull()
+        {
+            var queryable = new[] { 1, 2 }.AsQueryable();
+
+            Action singleElementOrThrowOnMultiple = () => queryable.SingleElementOrThrowOnMultiple(i => true, null);
+
+            singleElementOrThrowOnMultiple.ShouldThrow<ArgumentNullException>()
+                .Which.ParamName.ShouldBeEquivalentTo("multipleMatchesFoundException");
+        }
+
+        [Fact]
+        public void
+            SingleElementOrThrowOnMultipleByQueryWhenFoundShouldThrowArgumentNullExceptionWhenEnumerableArgumentIsNull()
+        {
+            Action singleElementOrThrowOnMultiple =
+                () => ((IQueryable<int>)null).SingleElementOrThrowOnMultiple(null, null);
+
+            singleElementOrThrowOnMultiple.ShouldThrowExactly<ArgumentNullException>()
+                .Which.ParamName.ShouldBeEquivalentTo("queryable");
+        }
+
+        [Fact]
+        public void
+            SingleElementOrThrowOnMultipleByQueryWhenFoundShouldThrowArgumentNullExceptionWhenPredicateArgumentIsNull()
+        {
+            Action singleElementOrThrowOnMultiple =
+                () => new int[0].AsQueryable().SingleElementOrThrowOnMultiple(null, null);
+
+            singleElementOrThrowOnMultiple.ShouldThrowExactly<ArgumentNullException>()
+                .Which.ParamName.ShouldBeEquivalentTo("predicate");
+        }
     }
 }
--- tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/SingleOrTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/QueryableExtensions/SingleOrTests.cs
@@ -1,6 +1,89 @@
 ﻿namespace Omego.Extensions.Tests.Unit.QueryableExtensions
 {
+    using System;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Omego.Extensions.QueryableExtensions;
+
+    using Xunit;
+
     public class SingleOrTests
     {
+        public void SingleOrShouldReturnElementIfOneExists()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.SingleOr(null).Should().Be("1");
+        }
+
+        [Fact]
+        public void SingleOrByQueryShouldReturnElementWhenFound()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.SingleOr(x => x == "1", null).Should().Be("1");
+        }
+
+        [Fact]
+        public void SingleOrByQueryShouldReturnRequestedDefaultObjectWhenQueryIsNotFound()
+        {
+            var queryable = new[] { "1" }.AsQueryable();
+
+            queryable.SingleOr(x => x == "2", "3").Should().Be("3");
+        }
+
+        [Fact]
+        public void SingleOrByQueryShouldThrowArgumentNullExceptionWhenPredicateArgumentIsNullWhenSearching()
+        {
+            Action singleOr = () => new string[] { null }.AsQueryable().SingleOr(null, null);
+
+            singleOr.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("predicate");
+        }
+
+        [Fact]
+        public void SingleOrByQueryShouldThrowArgumentNullExceptionWhenQueryableArgumentIsNullWhenSearching()
+        {
+            Action singleOr = () => ((IQueryable<string>)null).SingleOr(x => false, null);
+
+            singleOr.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("queryable");
+        }
+
+        [Fact]
+        public void SingleOrByQueryShouldThrowInvalidOperationExceptionWhenMultipleElementsAreFound()
+        {
+            var queryable = new object[2].AsQueryable();
+
+            Action singleOr = () => queryable.SingleOr(o => o == null, (object)null);
+
+            singleOr.ShouldThrow<InvalidOperationException>().WithMessage("More than one match found for (o == null).");
+        }
+
+        [Fact]
+        public void SingleOrShouldReturnRequestedDefaultObjectWhenNoElementsAreFound()
+        {
+            var queryable = new object[0].AsQueryable();
+
+            queryable.SingleOr("3").Should().Be("3");
+        }
+
+        [Fact]
+        public void SingleOrShouldThrowArgumentNullExceptionWhenQueryableArgumentIsNull()
+        {
+            Action singleOr = () => ((IQueryable<string>)null).SingleOr(null);
+
+            singleOr.ShouldThrowExactly<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("queryable");
+        }
+
+        [Fact]
+        public void SingleOrShouldThrowInvalidOperationExceptionWhenMultipleElementsAreFound()
+        {
+            var queryable = new object[2].AsQueryable();
+
+            Action singleOr = () => queryable.SingleOr((string)null);
+
+            singleOr.ShouldThrow<InvalidOperationException>().WithMessage("More than one match found for true.");
+        }
     }
 }
