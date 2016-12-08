3acdcd16013c17a202c4b6b5b45b37b7243fb369
Oleg Kleyman <Omego2K@gmail.com>
2016-12-07 22:36:12 -0500

    add singleOr XML doc

--- core/Omego.Extensions/QueryableExtensions/SingleOr.cs
+++ core/Omego.Extensions/QueryableExtensions/SingleOr.cs
@@ -22,6 +22,34 @@
         ///     Thrown when the <paramref name="predicate" /> argument is null.
         /// </exception>
         public static T SingleOr<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, T @default)
+            => queryable.SingleOr(predicate, () => @default);
+
+        /// <summary>
+        ///     Returns a single element of an <see cref="IQueryable{T}" /> or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="queryable">The queryable to find the single element in.</param>
+        /// <param name="default">The object to return if no elements are found.</param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
+        public static T SingleOr<T>(this IQueryable<T> queryable, T @default) => queryable.SingleOr(arg => true, @default);
+
+        /// <summary>
+        ///     Returns a single element of an <see cref="IQueryable{T}" /> matching the given predicate or returns
+        ///     a requested default object of type <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="queryable">The queryable to find the single element in.</param>
+        /// <param name="predicate">The predicate to use to find a single match.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" />.</returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the <paramref name="predicate" /> argument is null.
+        /// </exception>
+        public static T SingleOr<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, Func<T> @default)
         {
             if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
@@ -36,27 +64,12 @@
         ///     a requested default object of type <typeparamref name="T" />.
         /// </summary>
         /// <param name="queryable">The queryable to find the single element in.</param>
-        /// <param name="default">The object to return if no elements are found.</param>
+        /// <param name="default">
+        /// The <see cref="Func{TResult}"/> of <typeparamref name="T"/> to retrieve the
+        /// default object to return if no elements are found.
+        /// </param>
         /// <typeparam name="T">The type of the object to return.</typeparam>
         /// <returns>An instance of <typeparamref name="T" />.</returns>
-        public static T SingleOr<T>(this IQueryable<T> queryable, T @default)
-        {
-            return queryable.SingleOr(arg => true, @default);
-        }
-
-        public static T SingleOr<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, Func<T> @default)
-        {
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            return queryable.SingleOrDefaultOrThrow(
-                predicate,
-                @default,
-                new InvalidOperationException($"More than one match found for {predicate.Body}."));
-        }
-
-        public static T SingleOr<T>(this IQueryable<T> queryable, Func<T> @default)
-        {
-            return queryable.SingleOr(arg => true, @default);
-        }
+        public static T SingleOr<T>(this IQueryable<T> queryable, Func<T> @default) => queryable.SingleOr(arg => true, @default);
     }
 }
