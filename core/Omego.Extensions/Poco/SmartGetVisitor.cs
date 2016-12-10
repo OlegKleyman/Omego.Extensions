9f0650e74c750814fa34872c3e0ae763fcf3cf9d
Oleg Kleyman <Omego2K@gmail.com>
2016-12-10 02:17:16 -0500

    add SmartGetVisitor

---
+++ core/Omego.Extensions/Poco/SmartGetVisitor.cs
@@ -0,0 +1,47 @@
+﻿namespace Omego.Extensions.Poco
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    public class SmartGetVisitor : ExpressionVisitor
+    {
+        private object target;
+
+        private readonly Queue<string> nameQueue;
+
+        public SmartGetVisitor(object target)
+        {
+            this.target = target;
+            nameQueue = new Queue<string>();
+        }
+        
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            base.VisitMember(node);
+
+            if (target == null)
+            {
+                return node;
+            }
+
+            var member = (PropertyInfo)node.Member;
+
+            target = member.GetValue(target, null);
+            nameQueue.Enqueue(node.Member.Name);
+
+            return node;
+        }
+
+        public void OnNull(Expression expression, Action<string> onNullCallBack)
+        {
+            Visit(expression);
+
+            if (nameQueue.Count > 0)
+            {
+                onNullCallBack(string.Join(".", nameQueue));
+            }
+        }
+    }
+}
---
+++ tests/unit/Omego.Extensions.Tests.Unit/Poco/SmartGetVisitorTests.cs
@@ -0,0 +1,81 @@
+﻿namespace Omego.Extensions.Tests.Unit.Poco
+{
+    using System;
+    using System.Collections;
+    using System.Linq.Expressions;
+
+    using FluentAssertions;
+
+    using Omego.Extensions.Poco;
+
+    using Xunit;
+
+    [CLSCompliant(false)]
+    public class SmartGetVisitorTests
+    {
+        public class SmartGetVisitorTestsTheories
+        {
+            public static IEnumerable OnNullShouldSetNullQualifiedNameToNullForPropertiesTheory = new object[]
+                                                                                                     {
+                                                                                                         new object[]
+                                                                                                             {
+                                                                                                                 new Test
+                                                                                                                     {
+                                                                                                                         Test1
+                                                                                                                             =
+                                                                                                                             new Test1
+                                                                                                                                 {
+                                                                                                                                     Test2
+                                                                                                                                         =
+                                                                                                                                         new Test2
+                                                                                                                                         (
+                                                                                                                                         )
+                                                                                                                                 }
+                                                                                                                     },
+                                                                                                                 (Expression<Func<Test, object>>)(test => test.Test1.Test2.TestString),
+                                                                                                                 "Test1.Test2.TestString"
+                                                                                                             },
+                                                                                                         new object[]
+                                                                                                             {
+                                                                                                                 new Test
+                                                                                                                     {
+                                                                                                                         Test1
+                                                                                                                             =
+                                                                                                                             new Test1()
+                                                                                                                     },
+                                                                                                                 (Expression<Func<Test, object>>)(test => test.Test1.Test2.TestString),
+                                                                                                                 "Test1.Test2"
+                                                                                                             }
+                                                                                                     };
+        }
+
+        [Theory]
+        [MemberData("OnNullShouldSetNullQualifiedNameToNullForPropertiesTheory", MemberType = typeof(SmartGetVisitorTestsTheories))]
+        public void OnNullShouldSetNullQualifiedNameToNullForProperties(Test target, Expression expression, string expected)
+        {
+            var visitor = GetVisitor(target);
+            
+            visitor.OnNull(expression, qualifiedName => qualifiedName.ShouldBeEquivalentTo(expected));
+        }
+
+        private SmartGetVisitor GetVisitor(object target)
+        {
+            return new SmartGetVisitor(target);
+        }
+
+        public class Test
+        {
+            internal Test1 Test1 { get; set; }
+        }
+
+        public class Test1
+        {
+            internal Test2 Test2 { get; set; }
+        }
+
+        public class Test2
+        {
+            internal string TestString { get; set; }
+        }
+    }
+}
