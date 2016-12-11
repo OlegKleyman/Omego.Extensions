b94b71bd7b6e72d0c52b7ed2de0359631f979cdc
Oleg Kleyman <Omego2K@gmail.com>
2016-12-11 12:15:40 -0500

    add SmartGetExtension method

---
+++ core/Omego.Extensions/NullExtensions/SmartGet.cs
@@ -0,0 +1,36 @@
+﻿namespace Omego.Extensions.NullExtensions
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using Omego.Extensions.Poco;
+
+    public static class ObjectExtensions
+    {
+        public static TResult SmartGet<TTarget, TOrig, TResult>(
+            this TTarget target,
+            Expression<Func<TTarget, TOrig>> qualifierPath,
+            Func<TOrig, TResult> result,
+            Func<string, Exception> exception)
+        {
+            var visitor = new SmartGetVisitor(target);
+
+            visitor.OnNull(
+                qualifierPath,
+                nullQualifier =>
+                    {
+                        if (exception == null) throw new ArgumentNullException(nameof(exception));
+
+                        var toThrow = exception(nullQualifier);
+
+                        if (toThrow == null) throw new InvalidOperationException("Exception to throw returned null.");
+
+                        throw exception(nullQualifier);
+                    });
+
+            if(result == null) throw new ArgumentNullException(nameof(result));
+
+            return result((TOrig)visitor.Current);
+        }
+    }
+}
---
+++ tests/unit/Omego.Extensions.Tests.Unit/NullExtensions/SmartGetTests.cs
@@ -0,0 +1,124 @@
+﻿namespace Omego.Extensions.Tests.Unit.NullExtensions
+{
+    using System;
+    using System.Collections;
+    using System.Linq.Expressions;
+
+    using FluentAssertions;
+
+    using Omego.Extensions.NullExtensions;
+
+    using Xunit;
+
+    [CLSCompliant(false)]
+    public class SmartGetTests
+    {
+        [Fact]
+        public void SmartGetShouldReturnResultIfNothingIsNull()
+        {
+            var target = new Test { Test2 = new Test2 { Test3 = new Test3 { Something = "something" } } };
+
+            target.SmartGet(test => test.Test2.Test3, test3 => test3.Something, null).ShouldBeEquivalentTo("something");
+        }
+
+        [Fact]
+        public void SmartGetShouldThrowExceptionFromCallBackWhenSomethingIsNull()
+        {
+            var target = new Test { Test2 = new Test2() };
+
+            Action smartGet =
+                () =>
+                    target.SmartGet(
+                        test => test.Test2.Test3,
+                        test3 => test3.Something,
+                        s => new InvalidOperationException(s));
+
+            smartGet.ShouldThrow<InvalidOperationException>().WithMessage("Test2.Test3");
+        }
+
+        public class Test
+        {
+            internal Test2 Test2 { get; set; }
+        }
+
+        public class Test2
+        {
+            internal Test3 Test3;
+        }
+
+        public class Test3
+        {
+            internal string Something;
+        }
+
+        [Theory]
+        [MemberData("SmartGetShouldThrowArgumentExceptionWhenRequiredArgumentsAreInvalidTheory",
+             MemberType = typeof(SmartGetTestsTheories))]
+        public void SmartGetShouldThrowArgumentExceptionWhenRequiredArgumentsAreInvalid(
+            string message,
+            string parameterName,
+            Type exceptionType,
+            Test target,
+            Expression<Func<Test, Test3>> nullCheckExpression,
+            Func<Test3, object> returnExpression,
+            Func<string, Exception> onNullException)
+        {
+            Action smartGet = () => target.SmartGet(nullCheckExpression, returnExpression, onNullException);
+
+            smartGet.ShouldThrow<ArgumentException>()
+                .WithMessage(message)
+                .Where(
+                    exception => exception.ParamName == parameterName,
+                    "the parameter name should be of the problematic parameter")
+                .And.Should()
+                .BeOfType(exceptionType);
+        }
+
+        [Fact]
+        public void SmartGetShouldThrowInvalidOperationExceptionWhenExceptionRetrieverFunctionReturnsNull()
+        {
+            var target = new Test { Test2 = new Test2() };
+
+            Action smartGet = () => target.SmartGet(test => test.Test2.Test3, test3 => test3.Something, s => null);
+
+            smartGet.ShouldThrow<InvalidOperationException>().WithMessage("Exception to throw returned null.");
+        }
+
+        public class SmartGetTestsTheories
+        {
+            public static IEnumerable SmartGetShouldThrowArgumentExceptionWhenRequiredArgumentsAreInvalidTheory = new[]
+                                                                                                               {
+                                                                                                                   new object
+                                                                                                                       [
+                                                                                                                       ]
+                                                                                                                           {
+                                                                                                                               "Value cannot be null.\r\nParameter name: result",
+                                                                                                                               "result",
+                                                                                                                               typeof
+                                                                                                                               (
+                                                                                                                                   ArgumentNullException
+                                                                                                                               ),
+                                                                                                                               null,
+                                                                                                                               null,
+                                                                                                                               null,
+                                                                                                                               null
+                                                                                                                           },
+                                                                                                                   new object
+                                                                                                                       [
+                                                                                                                       ]
+                                                                                                                           {
+                                                                                                                               "Value cannot be null.\r\nParameter name: exception",
+                                                                                                                               "exception",
+                                                                                                                               typeof
+                                                                                                                               (
+                                                                                                                                   ArgumentNullException
+                                                                                                                               ),
+                                                                                                                               new Test(), 
+                                                                                                                               (Expression<Func<Test, Test3>>)(test => test.Test2.Test3),
+                                                                                                                               null,
+                                                                                                                               null
+                                                                                                                           }
+                                                                                                               };
+        }
+    }
+}
