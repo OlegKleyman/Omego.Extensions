b74630a828d00a93462eedf30428c3b0deb189e5
Oleg Kleyman <Omego2K@gmail.com>
2017-03-13 17:30:38 -0400

    run cleanup

--- core/Omego.Extensions/Poco/GenericEqualityComparer.cs
+++ core/Omego.Extensions/Poco/GenericEqualityComparer.cs
@@ -3,12 +3,28 @@ namespace Omego.Extensions.Poco
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    ///     Represents a generic equality comparer comparer.
+    /// </summary>
+    /// <typeparam name="TSource">The type to use for comparison.</typeparam>
     public class GenericEqualityComparer<TSource> : IEqualityComparer<TSource>
     {
         private readonly Func<TSource, TSource, bool> areEqual;
 
         private readonly Func<TSource, int> hashCode;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GenericEqualityComparer{TSource}" /> of
+        ///     <typeparamref name="TSource" /> struct.
+        /// </summary>
+        /// <param name="areEqual">
+        ///     The <see cref="Func{T1, T2, TResult}" /> of <typeparamref name="TSource" />,
+        ///     <typeparamref name="TSource" />, and <see cref="bool" /> to use for equality comparison.
+        /// </param>
+        /// <param name="hashCode">
+        ///     The <see cref="Func{T1, TResult}" /> of <typeparamref name="TSource" />,
+        ///     and <see cref="int" /> to use to get the hash code.
+        /// </param>
         public GenericEqualityComparer(Func<TSource, TSource, bool> areEqual, Func<TSource, int> hashCode)
         {
             this.areEqual = areEqual ??
@@ -17,8 +33,10 @@ namespace Omego.Extensions.Poco
             throw new ArgumentNullException(nameof(hashCode));
         }
 
+        /// <inheritdoc />
         public bool Equals(TSource x, TSource y) => areEqual(x, y);
 
+        /// <inheritdoc />
         public int GetHashCode(TSource obj) => hashCode(obj);
     }
 }
