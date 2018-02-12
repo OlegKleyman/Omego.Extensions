4bf43282f4a1d1d8aab46334aace59cf4c77663f
Omego2K <omego2k@gmail.com>
2018-02-12 08:48:52 -0500

    Add DbContext extension to add entity object and save to data store

---
+++ core/Omego.Extensions/DbContextExtensions/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Omego.Extensions.DbContextExtensions
+{
+    public static class DbContext
+    {
+        public static void AddRangeAndSave(this Microsoft.EntityFrameworkCore.DbContext context, params object[] entities)
+        {
+            (context ?? throw new ArgumentNullException(nameof(context))).AddRange(entities);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ tests/unit/Omego.Extensions.Tests.Unit/DbContextExtensions/DbExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Context = Microsoft.EntityFrameworkCore.DbContext;
+using NSubstitute;
+using Omego.Extensions.DbContextExtensions;
+using Omego.Extensions.Tests.Unit.DbContextExtensions.Support;
+using Xunit;
+
+namespace Omego.Extensions.Tests.Unit.DbContextExtensions
+{
+    public class DbExtensionsTests
+    {
+        [Fact]
+        public void AddRangeAndSaveShouldSaveEntityToDataStore()
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            using (var context = new MockContext(options))
+            {
+                context.AddRangeAndSave(new MockEntity{Test = "testing123"});
+            }
+
+            using (var context = new MockContext(options))
+            {
+                context.MockEntities.Should().Contain(entity => entity.Test == "testing123");
+            }
+        }
+
+        [Fact]
+        public void AddRangeAndSaveShouldThrowArgumentNullExceptionWhenContextIsNull()
+        {
+            Action addRangeAndSave = () => ((Context) null).AddRangeAndSave(null);
+
+            addRangeAndSave.ShouldThrow<ArgumentNullException>().Which.ParamName.ShouldBeEquivalentTo("context");
+        }
+    }
+}
