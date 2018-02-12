6e8edd64dea22fd07184d924ad4200d6ea37f1e8
Omego2K <omego2k@gmail.com>
2018-02-12 09:48:06 -0500

    return the number of objects written to database

--- core/Omego.Extensions/DbContextExtensions/DbContext.cs
+++ core/Omego.Extensions/DbContextExtensions/DbContext.cs
@@ -4,12 +4,12 @@ namespace Omego.Extensions.DbContextExtensions
 {
     public static class DbContext
     {
-        public static void AddRangeAndSave(this Microsoft.EntityFrameworkCore.DbContext context,
+        public static int AddRangeAndSave(this Microsoft.EntityFrameworkCore.DbContext context,
             params object[] entities)
         {
             (context ?? throw new ArgumentNullException(nameof(context))).AddRange(entities);
 
-            context.SaveChanges();
+            return context.SaveChanges();
         }
     }
 }
--- tests/unit/Omego.Extensions.Tests.Unit/DbContextExtensions/DbExtensionsTests.cs
+++ tests/unit/Omego.Extensions.Tests.Unit/DbContextExtensions/DbExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Omego.Extensions.DbContextExtensions;
@@ -23,6 +24,27 @@ namespace Omego.Extensions.Tests.Unit.DbContextExtensions
             using (var context = new MockContext(options))
             {
                 context.MockEntities.Should().Contain(entity => entity.Test == "testing123");
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void AddRangeAndSaveShouldReturnTheNumberOfObjectsWrittenToDatabase(int numberOfObjects)
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            using (var context = new MockContext(options))
+            {
+                var objects = new List<object>();
+
+                for (int i = 0; i < numberOfObjects; i++)
+                {
+                    objects.Add(new MockEntity());
+                }
+
+                context.AddRangeAndSave(objects.ToArray()).ShouldBeEquivalentTo(numberOfObjects);
             }
         }
 
