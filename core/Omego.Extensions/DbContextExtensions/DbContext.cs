8d08328d5a948a58f3fbea5fc512ca7b7256c79a
Omego2K <omego2k@gmail.com>
2018-02-12 09:53:59 -0500

    add XML documentation to the DbContext class

--- core/Omego.Extensions/DbContextExtensions/DbContext.cs
+++ core/Omego.Extensions/DbContextExtensions/DbContext.cs
@@ -2,8 +2,18 @@
 
 namespace Omego.Extensions.DbContextExtensions
 {
+    /// <summary>
+    ///     Contains extension methods for <see cref="Microsoft.EntityFrameworkCore.DbContext" />.
+    /// </summary>
     public static class DbContext
     {
+        /// <summary>
+        /// Adds entities to context and saves.
+        /// </summary>
+        /// <param name="context">The <see cref="Microsoft.EntityFrameworkCore.DbContext"/> to add
+        /// entities to and use to save to the database.</param>
+        /// <param name="entities">The entities to add and save.</param>
+        /// <returns>The number of objects written to the underlying database.s</returns>
         public static int AddRangeAndSave(this Microsoft.EntityFrameworkCore.DbContext context,
             params object[] entities)
         {
