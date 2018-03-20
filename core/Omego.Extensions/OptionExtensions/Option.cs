5ce48a6bc49765c074977dee39b961258dd1d56a
Omego2K <omego2k@gmail.com>
2018-03-20 06:34:40 -0400

    add Option extensions

---
+++ core/Omego.Extensions/OptionExtensions/Option.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Optional;
+
+namespace Omego.Extensions.OptionExtensions
+{
+    public static class Option
+    {
+        /// <summary>
+        ///     Evaluates a specified action if a value is present.
+        /// </summary>
+        /// <typeparam name="T">The type of the wrapped va;ue.</typeparam>
+        /// <param name="option">The <see cref="Option{T}" /> to match on.</param>
+        /// <param name="action">The action to evaluate if the value is present.</param>
+        /// <returns>The same <see cref="Option{T}" /> that was matched on.</returns>
+        public static Option<T> MatchSomeContinue<T>(this Option<T> option, Action<T> action)
+        {
+            option.MatchSome(action);
+
+            return option;
+        }
+
+        /// <summary>
+        ///     Evaluates a specified action if a value is present.
+        /// </summary>
+        /// <typeparam name="T">The type of the wrapped va;ue.</typeparam>
+        /// <param name="option">The <see cref="Option{T}" /> to match on.</param>
+        /// <param name="action">The action to evaluate if the value is present.</param>
+        /// <returns>The same <see cref="Option{T}" /> that was matched on.</returns>
+        public static Option<T> MatchNoneContinue<T>(this Option<T> option, Action action)
+        {
+            option.MatchNone(action);
+
+            return option;
+        }
+    }
+}
---
+++ tests/unit/Omego.Extensions.Tests.Unit/OptionExtensions/OptionTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using Omego.Extensions.OptionExtensions;
+using Xunit;
+using Option = Optional.Option;
+
+namespace Omego.Extensions.Tests.Unit.OptionExtensions
+{
+    public class OptionTests
+    {
+        [Fact]
+        public void MatchNoneContinueShouldExecuteActionWithTheSameValue()
+        {
+            var option = Option.None<object>();
+
+            var action = Substitute.For<Action>();
+
+            option.MatchNoneContinue(action);
+
+            action.Received()();
+        }
+
+        [Fact]
+        public void MatchNoneContinueShouldReturnTheSameOptionValue()
+        {
+            var option = Option.None<object>();
+
+            option.MatchNoneContinue(Substitute.For<Action>()).Should().BeEquivalentTo(option);
+        }
+
+        [Fact]
+        public void MatchSomeContinueShouldExecuteActionWithTheSameValue()
+        {
+            var testValue = new object();
+
+            var option = Option.Some(testValue);
+
+            var action = Substitute.For<Action<object>>();
+
+            option.MatchSomeContinue(action);
+
+            action.Received()(testValue);
+        }
+
+        [Fact]
+        public void MatchSomeContinueShouldReturnTheSameOptionValue()
+        {
+            var option = Option.Some(default(object));
+
+            option.MatchSomeContinue(Substitute.For<Action<object>>()).Should().BeEquivalentTo(option);
+        }
+    }
+}
